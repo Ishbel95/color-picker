f5a92b8bccf2ff069e26055d687f96533024c152
Ishbel95 <ishbel95@hotmail.com>
2022-08-31 10:19:04 +0100

    rendered hex code for colors, flexbox for color  scheme result

--- index.js
+++ index.js
@@ -2,17 +2,28 @@ const color = document.getElementById("colorValue");
 const mode = document.getElementById("mode");
 const form = document.getElementById("form");
 const result = document.getElementById("colorSchemeResult");
+
 let html = "";
 
 function renderColorScheme(data) {
+  let colorArr = data.colors;
+  for (let hex of colorArr) {
+    html = `
+            <div class="color-scheme-content">
+             <img src="${hex.image.named}"/>
+            <p>${hex.hex.value}</p>
+            <div>
+            `;
+    result.innerHTML += html;
+  }
+
+  console.log(colorArr);
   console.log(data);
-  html = `<img src="${data.image.named}"/>`;
-  result.innerHTML = html;
 }
 
 function getColorScheme(colorData) {
   fetch(
-    `https://www.thecolorapi.com/scheme?hex=${colorData.colorValue}&format=json&mode=${colorData.modeValue}&count=6`
+    `https://www.thecolorapi.com/scheme?hex=${colorData.colorValue}&format=json&mode=${colorData.modeValue}&count=5`
   )
     .then((response) => response.json())
     .then((data) => {
@@ -22,9 +33,6 @@ function getColorScheme(colorData) {
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
-
-  console.log(color.value);
-  console.log(mode.value);
 
   const data = {
     colorValue: color.value.substring(1),
